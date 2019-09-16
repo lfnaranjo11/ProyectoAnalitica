62bdc21519d2d39586b4a388c1058c9be2d19a7d
leonel naranjo <lf.naranjo11@uniandes.edu.co>
2019-09-16 18:00:08 -0500

    initial commit

---
+++ front/src/components/Component1/Component1.jsx
@@ -0,0 +1,16 @@
+import React,{Component}  from 'react';
+import './Component1.css';
+
+class Component1 extends Component{
+
+
+    render(){
+        return(
+        <h1> HOLA MAJO
+        </h1>
+        )
+    }   
+
+
+}
+export default Component1;
