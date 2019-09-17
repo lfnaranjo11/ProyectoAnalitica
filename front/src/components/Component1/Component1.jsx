d40954f277e9384445e2ecbe14f986592c2af5ba
lfnaranjo11 <lf.naranjo11@uniandes.edu.co>
2019-09-17 12:17:43 -0500

    back y front conectado sin proxy

--- front/src/components/Component1/Component1.jsx
+++ front/src/components/Component1/Component1.jsx
@@ -1,13 +1,35 @@
 import React,{Component}  from 'react';
 import './Component1.css';
+import axios from "axios";
+
 
 class Component1 extends Component{
+
+    constructor(props){
+        super(props);
+        this.state ={ NOMBRES:[] };
+    }
+
+
+    componentDidMount(){
+        let promesa = axios.get("http://localhost:3001/basicCRUD/",{
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded"
+            }
+          });
+          promesa.then(res => {
+             this.setState({NOMBRES:res.data});
+          });
+          promesa.catch(()=> console.log("pailas. serividor desconectado"));
+          
+        }
 
 
     render(){
         return(
-        <h1> HOLA MAJO
+        <h1> HOLA MAJo
         </h1>
+        
         )
     }   
 
